ada26b76cac4a0901c8c3ce2bf969b3f9d003d2f
Dhruv <deri361@aucklanduni.ac.nz>
2024-05-08 15:46:30 +1200

    Passed 6/10 test cases by taking in user input and outputting hand info

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,14 +6,20 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
   private int RoundNumber = 1;
+  private String name;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
-    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    name = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(name);
   }
 
   public void play() {
+    String input;
     MessageCli.START_ROUND.printMessage(String.valueOf(RoundNumber));
     RoundNumber++;
+    System.out.println(MessageCli.ASK_INPUT);
+    input = Utils.scanner.nextLine();
+    MessageCli.PRINT_INFO_HAND.printMessage(name, input);
   }
 
   public void endGame() {}
